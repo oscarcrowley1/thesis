c965d2c488e36c5166e95ea92b4709783c6ec057
crowleos <crowleos@tcd.ie>
2022-02-09 18:27:22 +0000

    small changes to load func

--- STGCN-PyTorch-master/utils.py
+++ STGCN-PyTorch-master/utils.py
@@ -8,24 +8,24 @@ def print_save(file, string_to_use):
     file.write(string_to_use + "\n")
 
 def load_scats_data():
-    # if (not os.path.isfile("STGCN-PyTorch-master/data/adj_mat.npy")
-    #         or not os.path.isfile("STGCN-PyTorch-master/data/node_values.npy")):
-    #     with zipfile.ZipFile("STGCN-PyTorch-master/data/METR-LA.zip", 'r') as zip_ref:
-    #         zip_ref.extractall("STGCN-PyTorch-master/data/")
-
-    # A = np.load("STGCN-PyTorch-master/data/adj_mat.npy")
-    # X = np.load("STGCN-PyTorch-master/data/node_values.npy").transpose((1, 2, 0))
-    # X = X.astype(np.float32)
-    
-    if (not os.path.isfile("STGCN-PyTorch-master/data/adj_mat_alpha.npy")
-            or not os.path.isfile("STGCN-PyTorch-master/data/node_values_alpha.npy")):
-        with zipfile.ZipFile("STGCN-PyTorch-master/data/SCATS.zip", 'r') as zip_ref:
+    if (not os.path.isfile("STGCN-PyTorch-master/data/adj_mat.npy")
+            or not os.path.isfile("STGCN-PyTorch-master/data/node_values.npy")):
+        with zipfile.ZipFile("STGCN-PyTorch-master/data/METR-LA.zip", 'r') as zip_ref:
             zip_ref.extractall("STGCN-PyTorch-master/data/")
-    
-    A = np.load("STGCN-PyTorch-master/data/interpret_csv/adj_mat_alpha.npy")
-    A = A.astype(np.float32)
-    X = np.load("STGCN-PyTorch-master/data/interpret_csv/node_values_alpha.npy").transpose((1, 2, 0))
+
+    A = np.load("STGCN-PyTorch-master/data/adj_mat.npy")
+    X = np.load("STGCN-PyTorch-master/data/node_values.npy").transpose((1, 2, 0))
     X = X.astype(np.float32)
+    
+    # if (not os.path.isfile("STGCN-PyTorch-master/data/adj_mat_alpha.npy")
+    #         or not os.path.isfile("STGCN-PyTorch-master/data/node_values_alpha.npy")):
+    #     with zipfile.ZipFile("STGCN-PyTorch-master/data/SCATS.zip", 'r') as zip_ref:
+    #         zip_ref.extractall("STGCN-PyTorch-master/data/")
+    
+    # A = np.load("STGCN-PyTorch-master/data/interpret_csv/adj_mat_alpha.npy")
+    # A = A.astype(np.float32)
+    # X = np.load("STGCN-PyTorch-master/data/interpret_csv/node_values_alpha.npy").transpose((1, 2, 0))
+    # X = X.astype(np.float32)
 
     # info_string += X.shape)
     # X = X[:, 0, :] # Flow only for predictions
