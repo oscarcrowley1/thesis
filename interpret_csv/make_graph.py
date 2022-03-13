d5fdf549a896a635a68155349f6d15b4989573fd
crowleos <crowleos@tcd.ie>
2022-03-13 16:53:42 +0000

    lots of work for presentation

--- interpret_csv/make_graph.py
+++ interpret_csv/make_graph.py
@@ -1,12 +1,17 @@
+from cmath import inf
 from turtle import color
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
-adj_matrix = np.load("interpret_csv/adj_mat_alpha.npy")
+adj_matrix = np.load("interpret_csv/adj_mat_alpha_0603.npy")
 
-graph = nx.from_numpy_matrix(adj_matrix)
+#adj_matrix = np.transpose(adj_matrix)
+
+graph = nx.from_numpy_matrix(adj_matrix, parallel_edges=True, create_using=nx.DiGraph)
+
+#graph = nx.reverse(graph)
 
 # for from_node in range(adj_matrix)
 
@@ -29,23 +34,24 @@ node_labels = {
 }
 
 node_pos = {
-    0: (0,0),
-    1: (1,1),
+    0: (1,0.25),
+    1: (1,1.5),
     2: (-3,-1),
-    3: (-3,1),
-    4: (-2,0),
-    5: (3.5,-4),
-    6: (3.5,-3),
-    7: (4,4),
-    8: (3,3.5),
+    3: (-3,1.5),
+    4: (-2,0.25),
+    5: (3,-4),
+    6: (4,-3),
+    7: (4,3.5),
+    8: (3,4.5),
 }
 
+plt.set_cmap(plt.get_cmap("YlGnBu"))
 
-
-graph_drawing = nx.draw_networkx(graph, pos=node_pos, with_labels=True, arrows=True, edge_color=info, edge_cmap=plt.get_cmap("viridis"))
+graph_drawing = nx.draw_networkx(graph, pos=node_pos, with_labels=True, arrows=True, edge_color=info, edge_cmap=plt.get_cmap("YlGnBu"))
+dummy_plot = plt.scatter(inf, inf, cmap="Blues") # plt.get_cmap("YlGnBu"))
 
 # heatmap = plt.pcolor(info,cmap=plt.get_cmap("viridis"))
 
-
-# plt.colorbar(plt.get_cmap("viridis"))
+plt.title("Kernelised Graph Structure")
+plt.colorbar(dummy_plot)# plt.get_cmap("YlGnBu"))
 plt.show()
